5144f6203cae622f212b0cc0b6d9b57a33975b80
11KJD8 <kyle.delaney0@gmail.com>
2017-02-15 13:22:40 -0500

    Update SlicerScripts.py

--- SlicerScripts.py
+++ SlicerScripts.py
@@ -1,10 +1,24 @@
 import vtk, qt, ctk, slicer
-from slicer.ScriptedLoadableModule import *
 
+
+#Jan 19
 def buttonClicked():
     print "Button clicked"
 
-    
 button = qt.QPushButton('Button')
 button.connect('clicked()', buttonClicked)
 button.show()
+
+
+#Jan 24
+#This will create a linear transform node
+modelToRas = slicer.vtkMRMLLinearTransformNode()
+modelToRas.SetName('ModelToRas')
+slicer.mrmlScene.AddNode(modelToRas)
+
+modelToRasTransform = vtk.vtkTransform()
+modelToRasTransform.PreMultiply() 
+modelToRasTransform.Update()
+
+modelToRas.SetAndObserveTransformToParent(modelToRasTransform)
+
